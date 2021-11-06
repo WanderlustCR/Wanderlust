d2aaead77be4bc86bafc64a7256ef8708136bddf
WanderlustCR <87618122+WanderlustCR@users.noreply.github.com>
2021-11-05 19:05:10 -0600

    bs changes error

---
+++ js/Bs.js
@@ -0,0 +1,16 @@
+function BuscarCoti(){
+  window.location.href="Coti/index.html#"+document.getElementById('name').value;
+}
+
+window.addEventListener('DOMContentLoaded', async (e) =>{ //DOMContentLoaded
+  //const querySnapshot = await getTask(); cada que actualiza
+
+
+  var name = window.location.href;
+     console.log(name);
+     if(name == "https://www.thewanderlustcr.com/#Exito"){
+      alert("Email enviado exitosamente / Email sent successfully");
+  }
+
+
+})
